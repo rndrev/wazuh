7436a9c1f6e29d068a45c0ed9f992b470c276bb6
Alberto Marin <alberto.marin@wazuh.com>
2018-04-10 14:16:51 -0700

    Update send response function

--- src/wazuh_modules/wmodules.h
+++ src/wazuh_modules/wmodules.h
@@ -136,8 +136,8 @@ int wm_relative_path(const char * path);
 #ifndef WIN32
 // Com request thread dispatcher
 void * wmcom_main(void * arg);
-size_t wmcom_dispatch(char *command, size_t length __attribute__ ((unused)), char *output);
-size_t wmcom_getconfig(const char * section, char * output);
+size_t wmcom_dispatch(char * command, char ** output);
+size_t wmcom_getconfig(const char * section, char ** output);
 #endif
 
 #endif // W_MODULES
